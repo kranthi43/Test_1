89898fb8fdf82ccc813124398d8cc9256f0c88c3
Kranthi Kiran <kranthi43@gmail.com>
2019-05-13 13:47:10 +0530

    Kranthi 121

--- src/Test.java
+++ src/Test.java
@@ -14,6 +14,7 @@ public class Test {
 		}catch (Exception e) {
 			System.out.println("MM");
 		}*/
+		System.out.println(MessageFormat.format("GGGGGGnumbe r Kiran 1 3 1 Hi");
 		System.out.println(MessageFormat.format("GGGGGGnumbe r Kiran 1 3 1 Kiran");
 		System.out.println(MessageFormat.format("GGGGGGnumbe r Kiran 1 3 Kranthi");
 		 final String dir = System.getProperty("user.dir");
